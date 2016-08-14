0cf171ac6d3538dc36dd26ff8a0402ac756ec689
mijail peralta <mijail peralta>
2016-08-14 15:22:24 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp')
+var babel = require('babelify')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var rename = require('gulp-rename')
+var watchify = require('watchify')
+
+var config = {
+	js: {
+		entry: 'client/index.js',
+		dest: 'public'
+	}
+}
+
+function compile (watch) {
+	var bundle = browserify(config.js.entry, { debug: true })
+
+	if (watch) {
+		bundle = watchify(bundle)
+		bundle.on('update', function updateBundle () {
+			console.log('Bundling...')
+			rebundle()
+		})
+	}
+
+	function rebundle () {
+		bundle
+			.transform(babel)
+			.bundle()
+			.on('error', function errorBundle (err) { console.log(err) })
+			.pipe(source(config.js.entry))
+			.pipe(rename('app.js'))
+			.pipe(gulp.dest(config.js.dest))
+	}
+	rebundle()
+}
+
+gulp.task('build', function buildJS () {
+	compile()
+})
+
+gulp.task('build:watch', function buildJS () {
+	compile(true)
+})
+
+gulp.task('default', ['build:watch'])
